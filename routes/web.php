6355db210e363650f9b41b22e45a6402b6d781fd
orva06 <orvalamarva@student.telkomuniversity.ac.id>
2023-04-03 15:32:51 +0700

    css,js, dan routes ref #2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PageController;
+use Illuminate\Routing\RouteGroup;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +17,45 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('mainHome');
+});
+
+Route::get('/pages/perusahaan', function () {
+    return view('/pages/perusahaan');
+});
+
+Route::get('/pages/detailPerusahaan', function () {
+    return view('/pages/detailPerusahaan');
+});
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/mainHome', function () {
+    return view('/mainHome');
+});
+
+Route::post('/postlogin', [LoginController::class, "postlogin"])->name('postlogin');
+Route::get('/logout', [LoginController::class, "logout"]);
+
+//hanya untuk admin dan company
+Route::group(['middleware'=>['auth', 'ceklevel:Admin,Pelamar,Company']], function(){
+    Route::get('/home', function () {
+        return view('home');
+    });
+
+    // Route::get('/detailPerusahaan', [PageController::class, "detailPerusahaan"])->name('detailPerusahaan');
+    // Route::get('/perusahaan', [PageController::class, "perusahaan"])->name('perusahaan');
+});
+
+
+Route::group(['middleware'=>['auth', 'ceklevel:Pelamar']], function(){
+
+    Route::get('/detailPerusahaan', [PageController::class, "detailPerusahaan"])->name('detailPerusahaan');
+});
+
+Route::group(['middleware'=>['auth', 'ceklevel:Company']], function(){
+
+    Route::get('/perusahaan', [PageController::class, "perusahaan"])->name('perusahaan');
 });
