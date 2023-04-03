e7d50e03442ae56b4119e995a9fb1125ad535c6c
farisslmn <faris149b@gmail.com>
2023-04-03 17:03:58 +0700

    update logout ref #3

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,13 +11,13 @@ class LoginController extends Controller
     public function postlogin (Request $request){
         //dd($request->all());
         if (Auth::attempt($request->only('email','password'))){
-            return redirect('/cobaHome');
+            return redirect('/home');
         }
-        return redirect('login2');
+        return redirect('login');
     }
 
     public function logout (Request $request){
         Auth::logout();
-        return redirect('/login2');
+        return redirect('/login');
     }
 }
