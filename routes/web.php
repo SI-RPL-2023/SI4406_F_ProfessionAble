d65db0bfdc8818d862ac6ee1eff3793d3730eb87
azuraa61 <tiaraaz6194@gmail.com>
2023-05-20 13:21:46 +0700

    update web route history ref #11

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
-use App\Http\Controllers\applyController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\profileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\registerController;
 use App\Http\Controllers\jobController;
-
+use App\Http\Controllers\applyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,11 +25,16 @@ Route::get('/login', [LoginController::class, 'index']) -> name('login');
 Route::post('/postlogin', [LoginController::class, 'postlogin'])->name('postlogin');
 Route::get('/logout', [LoginController::class, 'logout']);
 
-Route::resource('profile', profileController::class);
+//Profile
+// Route::resource('profile', profileController::class);
+Route::get('/profile/{id}/editProfile', [registerController::class, "editProfile"]);
+Route::post('/profile/{id}', [registerController::class, "updateProfile"]);
+Route::get('/profile', [registerController::class, "viewProfile"]);
 
 Route::get('/home', function () {
     return view('home');
 })->name('home');
+
 
 Route::get('/nambahpekerjaan', function () {
     return view('nambahpekerjaan');
@@ -68,3 +72,7 @@ Route::get('/detailLowongan', [jobController::class, "job_detail"]);
 Route::get('/view/detail/{id}', [jobController::class, "viewPage_detailApply"]);
 Route::post('/detail/Lowongan/applyPage/{id}', [applyController::class, "showApplyPage"]);
 Route::post('/apply/job/', [applyController::class, "apply_store"]);
+
+//history
+Route::get('/riwayatlamaran/{id}', [applyController::class, "historyPage"]);
+Route::get('/detailriwayat/{id}', [applyController::class, "detailHistory"]);
