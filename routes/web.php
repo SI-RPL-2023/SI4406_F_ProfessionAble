30b71ae8184439cb60a26d521cbbfaff6deeb3de
alamandaregitautami <ualamanda@gmail.com>
2023-05-29 13:01:00 +0700

    melanjutkan pengerjaan pbi 13 ref #13

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
-use Illuminate\Routing\RouteGroup;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\profileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\registerController;
+use App\Http\Controllers\jobController;
+use App\Http\Controllers\applyController;
+use App\Http\Controllers\adminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,65 +14,74 @@ use App\Http\Controllers\registerController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', function () {
-    return view('mainHome');
-});
+Route::get('/', [registerController::class, 'view']) -> name('register');
+Route::post('/', [registerController::class, 'registerStore']);
 
-Route::get('/pages/perusahaan', function () {
-    return view('/pages/perusahaan');
-});
+Route::get('/login', [LoginController::class, 'index']) -> name('login');
+Route::post('/postlogin', [LoginController::class, 'postlogin'])->name('postlogin');
+Route::get('/logout', [LoginController::class, 'logout']);
 
-Route::get('/pages/detailPerusahaan', function () {
-    return view('/pages/detailPerusahaan');
-});
+//Profile
+// Route::resource('profile', profileController::class);
+Route::get('/profile/{id}/editProfile', [profileController::class, "editProfile"]);
+Route::post('/profile/update/{id}', [profileController::class, "updateProfile"]);
+Route::get('/profile', [profileController::class, "viewProfile"]);
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
-// Register Start
-
-Route::get('/register', function () {
-    return view('/register');
-})->name('register');
-
-Route::post('/register/store', [registerController::class, "register_store"]);
-
-// Register ends here
-
-Route::get('/mainHome', function () {
-    return view('/mainHome');
-});
-
-Route::post('/postlogin', [LoginController::class, "postlogin"])->name('postlogin');
-
-//ini buat logout dan sudah done
-Route::get('/logout', [LoginController::class, "logout"]);
-
-//hanya untuk admin dan company
-Route::group(['middleware'=>['auth', 'ceklevel:Admin,Pelamar,Company']], function(){
-    Route::get('/home', function () {
-        return view('home');
-    });
-
-    // Route::get('/detailPerusahaan', [PageController::class, "detailPerusahaan"])->name('detailPerusahaan');
-    // Route::get('/perusahaan', [PageController::class, "perusahaan"])->name('perusahaan');
-});
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
 
 
-Route::group(['middleware'=>['auth', 'ceklevel:Pelamar']], function(){
+Route::get('/nambahpekerjaan', function () {
+    return view('nambahpekerjaan');
+})->name('nambahpekerjaan');
 
-    Route::get('/detailPerusahaan', [PageController::class, "detailPerusahaan"])->name('detailPerusahaan');
-});
+Route::get('/registrasilowongan', function () {
+    return view('registrasilowongan');
+})->name('registrasilowongan');
 
-Route::group(['middleware'=>['auth', 'ceklevel:Company']], function(){
 
-    Route::get('/perusahaan', [PageController::class, "perusahaan"])->name('perusahaan');
-});
+Route::get('/detailriwayat', function () {
+    return view('detailriwayat');
+})->name('detailriwayat');
 
-Route::get('profile', [profileController::class, "index"]);
+Route::get('/detaillowongan', function () {
+    return view('detaillowongan');
+})->name('detaillowongan');
+
+Route::get('/profileperusahaan/{id}', [profileController::class, "detail_perusahaan"]);
+
+
+//tambahan dr lala
+Route::get('/company/create', [jobController::class, "createJob_view"]);
+Route::get('/home', [jobController::class, "index"]);
+Route::post('/add/store', [jobController::class, "createJob_store"]);
+Route::get('/detailLowongan', [jobController::class, "job_detail"]);
+Route::get('/view/detail/{id}', [jobController::class, "viewPage_detailApply"]);
+Route::get('/view/detail/{id}', [jobController::class, "viewPage_detailApply"]);
+Route::post('/detail/Lowongan/applyPage/{id}', [applyController::class, "showApplyPage"]);
+Route::post('/apply/job/', [applyController::class, "apply_store"]);
+
+//history
+Route::get('/riwayatlamaran', [applyController::class, "historyPage"]);
+Route::get('/detailriwayat/{id}', [applyController::class, "detailHistory"]);
+
+//admin
+Route::get('/admin/pantauUser', function () {
+    return view('/admin/pantauUser');
+})->name('/admin/pantauUser');
+
+//tampil list user
+Route::get('/admin/pantauUser', [adminController::class, "listUser"]);
+//edit user
+Route::get('/admin/{id}/editUser', [adminController::class, "editUser"]);
+Route::post('/updateUser/{id}', [adminController::class, "updateUser"]);
+//hapus user
+Route::get('/admin/{id}/delete_akun', [adminController::class, "delete_akun"]);
+//search
+Route::get('/carilowongan', [jobController::class, "listSearch"])->name('carilowongan');
