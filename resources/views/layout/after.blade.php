f0d7f951c2e5a821bb312732b2a4e6cf602f7a3b
adnannaulih <adnannauliharahap@gmail.com>
2023-06-04 20:55:33 +0700

    view informasi umum ref #16

--- resources/views/layout/after.blade.php
+++ resources/views/layout/after.blade.php
@@ -10,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="dropdown.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('dropdown.css') }}">
     <style>
         a {
             text-decoration: none;
@@ -42,163 +42,93 @@
 </head>
 
 <body>
+    <main>
+        <div class="container-fluid px-0">
+            <div class="row">
+                <div class="col mb-5" style="background-image: url('head.png'); height: 550px; background-size: cover;">
+                    <div class="jumbotron">
+                        <nav class="navbar navbar-expand-lg mt-4 mx-5" style="background-color:transparent">
+                            <div class="container-fluid px-0">
+                                <a class="navbar-brand" href="#"><img src="logoApp.png" width="120px" alt=""></a>
+                                {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                                    aria-expanded="false" aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button> --}}
+                                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                    <div class="navbar-nav mx-auto me-5">
+                                        <a class="active mx-3" aria-current="page" href="/home">Home</a>
+                                            @if (auth()->user()->level == 'Pelamar')
+                                                <a href="/carilowongan">Cari Lowongan</a>
+                                            @elseif (auth()->user()->level == 'Company')
+                                                <a href="/nambahpekerjaan">Post Lowongan</a>
+                                            @endif
 
-    {{-- <nav class="navbar navbar-expand-lg bg-white">
-        <div class="container"> --}}
-    {{-- <div class="col">
-        <div class="jumbotron rounded"
-            style="background-image: url('/foto/head.png');
-                        height: 550px; background-repeat: no-repeat;"> --}}
+                                        <div class="dropdown mx-5">
+                                            <a href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                <i class="bi bi-bell-fill"></i>
+                                                <span class="badge rounded-pill badge-notification"></span>
+                                            </a>
+                                        </div>
 
-            <main>
-                <div class="container-fluid px-0">
-                    <div class="row">
-                        <div class="col mb-5"
-                            style="background-image: url('head.png');
-                height: 550px; background-size: cover;">
-                            <div class="jumbotron">
-                                <nav class="navbar navbar-expand-lg mt-4 mx-5" style="background-color:transparent">
-                                    <div class="container-fluid px-0">
-                                        <a class="navbar-brand" href="#"><img src="logoApp.png"
-                                                width="120px" alt=""></a>
-                                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                                            data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
-                                            aria-expanded="false" aria-label="Toggle navigation">
-                                            <span class="navbar-toggler-icon"></span>
-                                        </button>
-                                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                                            <div class="navbar-nav mx-auto me-5">
-                                                <a class="active mx-3" aria-current="page" href="/home">Home</a>
-                                                @if (auth()->user()->level == 'Pelamar')
-                                                    <a href="">Cari Lowongan</a>
-                                                @elseif (auth()->user()->level == 'Company')
-                                                    <a href="">Post Lowongan</a>
-                                                @endif
-                                                {{-- <a class="mx-3" href="#">Features</a> --}}
+                                        @if (Auth::user())
+                                            <img src="https://ui-avatars.com/api/?background=random&color=fff&name={{ Auth::user()->name }}" alt="" class="userPic" onclick="toggleMenu()">
+                                            <h5 class="my-2 mx-2">{{ Auth::user()->name }} ({{ Auth::user()->level }})</h5>
+                                        @endif
 
-
-
-
-                                                <div class="dropdown mx-5">
-                                                    <a class="dropdown-toggle" href="#" role="button"
-                                                        data-bs-toggle="dropdown" aria-expanded="false">
-                                                        <i class="bi bi-bell-fill"></i>
-                                                        <span
-                                                            class="badge rounded-pill badge-notification bg-danger">1</span>
-                                                    </a>
-
-                                                    {{-- <ul class="dropdown-menu dropdown-menu-end"
-                                                        aria-labelledby="navbarDropdownMenuLink">
-                                                        <li>
-                                                            <a class="dropdown-item" href="#">Some news</a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="dropdown-item" href="#">Another news</a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="dropdown-item" href="#">Something else
-                                                                here</a>
-                                                        </li>
-                                                    </ul> --}}
+                                        <div class="sub-menu-wrap" id="subMenu">
+                                            <div class="sub-menu">
+                                                <div class="user-info">
+                                                    <p>{{ Auth::user()->name }}</p>
                                                 </div>
-
-                                                @if (Auth::user())
-                                                    <img src="https://ui-avatars.com/api/?background=random&color=fff&name={{ Auth::user()->name }}"
-                                                        alt="" class="userPic" onclick="toggleMenu()">
-
-                                                    <h5 class="my-2 mx-2">{{ Auth::user()->name }}
-                                                        ({{ Auth::user()->level }})
-                                                    </h5>
+                                                <a 
+                                                    href="/profile"
+                                                class="sub-menu-link">
+                                                    <p>Profile</p>
+                                                    <span>></span>
+                                                </a>
+                                                @if (auth()->user()->level == "Pelamar")
+                                                <a href="/riwayatlamaran" class="sub-menu-link">
+                                                    <p>History</p>
+                                                    <span>></span>
+                                                </a>
+                                                @elseif (auth()->user()->level == "Company")
+                                                <a href="/melihatListPelamar" class="sub-menu-link">
+                                                    <p>Daftar Pelamar</p>
+                                                    <span>></span>
+                                                </a>
                                                 @endif
-                                                {{-- <button class="btn btn-light" onclick="toggleMenu()">Sign In</button> --}}
-                                                <div class="sub-menu-wrap" id="subMenu">
-                                                    <div class="sub-menu">
-                                                        <div class="user-info">
-                                                            <p>{{ Auth::user()->name }}</p>
-                                                        </div>
-                                                        <a href="/profile" class="sub-menu-link">
-                                                            <p>Profile</p>
-                                                            <span>></span>
-                                                        </a>
-
-                                                        <a href="/logout" class="sub-menu-link">
-                                                            <p>Logout</p>
-                                                            <span>></span>
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                </nav>
-                                <div class="container mt-5">
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="input-group w-50 mx-auto mt-5">
-                                                <input type="search" class="form-control opacity-50"
-                                                    placeholder="Search" aria-label="Search"
-                                                    aria-describedby="search-addon" />
-                                                <button type="button" class="btn btn-white">search</button>
+                                                <a href="/logout" class="sub-menu-link">
+                                                    <p>Logout</p>
+                                                    <span>></span>
+                                                </a>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </nav>
                     </div>
                 </div>
-            </main>
-
-
-
-
-
-            <!-- Avatar -->
-            {{-- <div class="dropdown">
-                        <a class="dropdown-toggle d-flex align-items-center hidden-arrow" href="#"
-                            id="navbarDropdownMenuAvatar" role="button" data-mdb-toggle="dropdown"
-                            aria-expanded="false">
-                            <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle dropdown-img"  alt="Black and White Portrait of a Man" loading="lazy" />
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
-                            <li>
-                                <a class="dropdown-item" href="#">My profile</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">Settings</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">Logout</a>
-                            </li>
-                        </ul>
-                    </div> --}}
+            </div>
         </div>
+    </main>
 
-
-        {{-- <div class="button">
-                        <a href="#" class="">Daftar</a>
-                    </div> --}}
-        {{-- <a class="button">Daftar</a> --}}
-    </div>
-    </div>
-    </div>
-    </nav>
-
-    {{-- <div class="container"> --}}
-        @yield('homePage')
-    {{-- </div> --}}
+    @yield('homePage')
 
 </body>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-    integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-    integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
-</script>
-<script>
-    let subMenu = document.getElementById('subMenu');
 
-    function toggleMenu() {
-        subMenu.classList.toggle('open-menu');
-    }
-</script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+    <script>
+        let subMenu = document.getElementById('subMenu');
 
+        function toggleMenu() {
+            subMenu.classList.toggle('open-menu');
+        }
+    </script>
 </html>
---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,108 @@
+@extends('layout.main')
+
+@section('content')
+
+<style>
+    .button {
+        background: #F47C46;
+        color: #FFFFFF;
+    }
+    .button:hover {
+      border-color: #F47C46;
+      color: #F47C46;
+    }
+    .button-outline {
+        border-style: ridge;
+        border-color: #f47c46;
+        color: #7c7c7c;
+    }
+    .button-outline:hover {
+        background: #F47C46;
+        color: #FFFFFF;
+    }
+    .shadow-oren:focus {
+        box-shadow: 0 0 0 .25rem rgba(244, 124, 70, .25);
+        border-color: rgba(244, 124, 70, .5);
+    }
+    img.atas {
+    min-height: 100%;
+    width: 100%;
+    height: auto;
+    max-width: 100%;
+    }
+</style>
+
+@include('layout.afternew')
+
+<div class="d-flex justify-content-center">
+    <img src="kursi_roda.png" alt="kursi_roda" class="atas">
+</div>
+
+<div class="container">
+    <div class="row text-center">
+        <div class="col mt-5 mb-3">
+            <h3 class="fw-bold">Tentang Kami</h3>
+        </div>
+    </div>
+    <div class="row text-center">
+        <div class="col">
+            <h5 class="fw-medium lh-base">ProfessionAble merupakan perusahaan yang bergerak dari tahun 2023 dengan memberikan layanan berupa penyedia informasi mengenai perusahaan yang membuka loongan pekerjaan untuk orang disabilitas. Sehingga, penggunanya dapat mendaftarkan diri ke perusahaan yang mereka impikan.</h5>
+        </div>
+    </div>
+    <div class="row text-center">
+        <div class="col mt-5 mb-4">
+            <h3 class="fw-bold">Fokus Kami</h3>
+        </div>
+    </div>
+    <div class="row text-center mb-5 mt-1">
+        <div class="col">
+            <div class="card text-bg-dark border border-0 shadow-lg">
+                <img src="speaker.jpg" class="card-img" alt="speaker" style="box-shadow: 0 0 15px .25rem rgba(255, 118, 54, 0.5);">
+                <div class="card-img-overlay">
+                  <h5 class="card-title text-start">Menyediakan <br> lowongan pekerjaan</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card text-bg-dark border border-0 shadow-lg">
+                <img src="jam.jpg" class="card-img" alt="jam" style="box-shadow: 0 0 12px .25rem #9e997b;">
+                <div class="card-img-overlay">
+                  <h5 class="card-title text-start">Tidak butuh waktu <br> lama mengakses</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card text-bg-dark border border-0 shadow-lg">
+                <img src="lampu.jpg" class="card-img" alt="lampu" height="355px" style="box-shadow: 0 0 15px .25rem #ebc2c0;">
+                <div class="card-img-overlay">
+                  <h5 class="card-title text-start">Memberikan informasi <br> up to date</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row text-center">
+        <div class="col mt-3 mb-4">
+            <h3 class="fw-bold">Kontribusi Terhadap SDGs</h3>
+        </div>
+    </div>
+    <div class="row mt-1 mb-4">
+        <div class="col-8 mb-4 d-flex align-items-center">
+            <h5 class="fw-medium lh-base" style="text-align: justify; text-indent: 65px;">Berkontribusi untuk mengurangi kemiskinan dan meningkatkan kesejahteraan rakyat. Dengan terciptanya lapangan kerja yang layak dan pertumbuhan ekonomi yang inklusif dan berkelanjutan, diharapkan akan mengarah pada akses yang lebih baik bagi semua orang terhadap pendidikan, kesehatan, dan utilitas publik lainnya. Ini akan mendorong pembangunan berkelanjutan dan menciptakan lebih banyak kemakmuran bagi masyarakat secara keseluruhan.</h5>
+        </div>
+        <div class="col text-center">
+            <img src="sdgs_8.png" alt="sdgs_8" width="250px" class="rounded-4 shadow-lg">
+        </div>
+    </div>
+    <div class="row mb-5">
+        <div class="col">
+            <img src="orang.jpg" alt="orang" width="500px">
+        </div>
+        <div class="col d-flex align-items-center justify-content-center flex-column">
+            <h1 class="text-center fw-bold text-black mx-auto">Ayo Lihat Pekerjaan <br> yang Tersedia</h1>
+            <a href="/carilowongan"><button class="btn button btn-lg my-3 fw-bold" style="width: 175px" type="button">Selengkapnya</button></a>
+        </div>
+    </div>
+</div>
+
+@include('layout.footer')
+@endsection
