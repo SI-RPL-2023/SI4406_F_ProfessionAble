909073af80bb02fa731fc21d8068d301d5ecc712
farisslmn <faris149b@gmail.com>
2023-06-04 17:40:58 +0700

    melanjutkan pengerjaan controller ref #12

--- app/Http/Controllers/jobController.php
+++ app/Http/Controllers/jobController.php
@@ -6,9 +6,11 @@ use Carbon\Carbon;
 use App\Models\Job;
 use App\Models\User;
 use App\Models\Apply;
+use App\Models\Artikel;
 use App\Models\Pelamar;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class jobController extends Controller
 {
@@ -16,7 +18,8 @@ class jobController extends Controller
     public function index()
     {
         $list = Job::all();
-        return view('/home', compact('list'));
+        $list_artikel = Artikel::all();
+        return view('/home', compact('list', 'list_artikel'));
     }
 
     //tambah job
@@ -83,7 +86,7 @@ class jobController extends Controller
     {
         $lowongan = Job::find($id);
         $lowongan->delete();
-        return redirect('company')->with('success', "Data berhasil dihapus");
+        return redirect('/home')->with('success', "Data berhasil dihapus");
     }
 
     //tampil halaman detail job sisi pelamar
@@ -91,16 +94,27 @@ class jobController extends Controller
     {
         $apply_job = Job::findOrFail($id);
         $lowongan_lain = Job::latest()->take(2)->get();
+        $total_lowongan = Job::latest()->count();
         $profile_company = User::all();
-        return view('detaillowongan', compact(['apply_job', 'lowongan_lain', 'profile_company']));
+        return view('detaillowongan', compact(['apply_job', 'lowongan_lain', 'profile_company', 'total_lowongan']));
     }
 
     //list cari lowongan
-    public function listSearch(Request $request)
+    public function listSearch()
     {
         return view('carilowongan', [
+            'total_lowongan'=> Job::latest()->filter(request(['search']))->count(),
             'lowongan_search' => Job::latest()->filter(request(['search']))->get(),
         ]);
+    }
+
+    public function listPelamar()
+    {
+        $total = Apply::where('nama_perusahaan', Auth::user()->name)->count();
+        $user = auth()->user();
+        // $job = Job::latest()->get();
+        $job = Job::where('user_id', Auth::user()->id)->get();
+        return view ('melihatListPelamar', compact(['job', 'total']));
     }
 
 }
